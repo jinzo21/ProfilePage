68550d2b4ca93ec4e1be37015f1ffb0f10c88fa2
aUrbina <v-alurbi@microsoft.com>
2019-01-14 21:08:23 -0800

    removed mdl

--- profile-page-react/src/App.js
+++ profile-page-react/src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { pokedex_getAll } from "./moqService";
 
 class App extends Component {
-  state = {
-    pokemon: []
-  }
-
-  async componentDidMount(){
-    const pokemon = await pokedex_getAll();
-    console.log(pokemon);
-    this.setState({pokemon}); 
-  }
-  
   render() {
-    const { pokemon } = this.state;
-
     return (
       <div>
-        { pokemon.map( pokemon => (
-          <div>{ pokemon.PokemonName }</div>
-          ))}
+        <h1>Hellow World</h1>
       </div>
     );
   }
