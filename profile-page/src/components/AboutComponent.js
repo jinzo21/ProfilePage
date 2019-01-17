81a1756338935cf60cdfc112b17231b590c563d6
aUrbina <v-alurbi@microsoft.com>
2019-01-16 22:25:40 -0800

    moved home info to about me - ready for parallax

--- profile-page/src/components/AboutComponent.js
+++ profile-page/src/components/AboutComponent.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import { Grid, Col, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Button, Row, Col, Image } from 'react-bootstrap';
 import './AboutComponent.css';
 
 class AboutComponent extends Component {
   render() {
     return (
-      <div>
-        <Image src="mediaFiles/astronaut1.jpg" className="header-image" responsive/>
-        <Grid>
-          <Col xs={12} sm={8} smOffset={2}>
-            <Image src="mediaFiles/astronaut1.jpg" className="about-profile-pic" responsive/>
-            <h3>Frank The Tank</h3>
-            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out. These trees are so much fun. I get started on them and I have a hard time stopping. How to paint. That's easy. What to paint. That's much harder. Be brave. The man who does the best job is the one who is happy at his job.</p>
-            <p>Anyone can paint. You can do anything here. So don't worry about it. Life is too short to be alone, too precious. Share it with a friend. Every highlight needs it's own personal shadow.</p>
-            <p>That's what makes life fun. That you can make these decisions. That you can create the world that you want. Of course he's a happy little stone, cause we don't have any other kind. It's so important to do something every day that will make you happy.</p>
-            <p>You better get your coat out, this is going to be a cold painting. That's the way I look when I get home late; black and blue. That's crazy. We're trying to teach you a technique here and how to use it.</p>
+      <Grid>
+        <Jumbotron>
+          <h2>Welcome to CodeLife</h2>
+          <p>This is how to build a website with React, React-Router & React-Bootstrap</p>
+          <Link to="/about">
+            <Button bsStyle="primary">Learn More</Button>
+          </Link>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="mediaFiles/astronaut1.jpg" circle className="profile-pic"/>
+            <h3>Frank</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
           </Col>
-        </Grid>
-      </div>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="mediaFiles/astronaut2.jpg" circle className="profile-pic"/>
+            <h3>Vanessa</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="mediaFiles/astronaut3.jpg" circle className="profile-pic"/>
+            <h3>Riff</h3>
+            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
